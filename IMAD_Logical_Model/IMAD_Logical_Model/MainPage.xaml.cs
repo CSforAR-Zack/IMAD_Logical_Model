03457b51505fac9505c573136f3e5e9a8e28d47b
Zackary <zspink@nea.k12.ar.us>
2022-06-02 13:07:07 -0500

    Functionality

--- IMAD_Logical_Model/IMAD_Logical_Model/MainPage.xaml.cs
+++ IMAD_Logical_Model/IMAD_Logical_Model/MainPage.xaml.cs
@@ -8,5 +8,46 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
 	}
+
+    void ChangeColor(object sender, EventArgs args)
+    {
+
+        try
+        {
+            Button button = (Button)sender;
+
+            if (button.Text == "Active")
+            {
+                button.SetDynamicResource(BackgroundColorProperty, "Inactive");
+                button.Text = "Inactive";
+            }
+            else
+            {
+                button.SetDynamicResource(BackgroundColorProperty, "Active");
+                button.Text = "Active";
+            }
+        }
+        catch (InvalidCastException e)
+        {
+
+        }
+
+        if (operatorPicker.SelectedItem == null)
+            return;
+
+        string selectedOperator = operatorPicker.SelectedItem.ToString();
+
+        if (selectedOperator == "AND")
+            resultLabel.Text = (leftButton.Text == "Active" && rightButton.Text == "Active").ToString();
+        else if (selectedOperator == "OR")
+            resultLabel.Text = (leftButton.Text == "Active" || rightButton.Text == "Active").ToString();
+        else if (selectedOperator == "XOR")
+            resultLabel.Text = (leftButton.Text == "Active" ^ rightButton.Text == "Active").ToString();
+
+        if (resultLabel.Text == "True")
+            resultLabel.SetDynamicResource(BackgroundColorProperty, "Active");
+        else
+            resultLabel.SetDynamicResource(BackgroundColorProperty, "Inactive");
+    }
 }
 
